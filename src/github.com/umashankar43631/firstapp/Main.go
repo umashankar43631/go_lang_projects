e264ea324a76df161348ddb4704c4e4def06646e
mulakalapalli shankar <shankar.mulakalapalli@gmail.com>
2020-06-21 23:34:45 +0530

    update2

--- src/github.com/umashankar43631/firstapp/Main.go
+++ src/github.com/umashankar43631/firstapp/Main.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var str1 = " Hi Shankar "
 
-	fmt.Println("Hello World!")
+	var x, y int = 20, 30
+	var z int //deafult value for all uninitialized variables in Go language is 0
+	fmt.Println("The values entered for x and y variables are : ", x, y, "Uninitialized value for variable z is : ", z)
+
 }
